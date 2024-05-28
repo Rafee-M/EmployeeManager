af4c283b2fc6943416f4e97b0a5c3f47dc3a2c2e
Raf <69535896+Rafee-M@users.noreply.github.com>
2024-05-28 11:45:58 +0000

    Add files via upload

---
+++ src/employeemanager/EmployeeManager.java
@@ -0,0 +1,14 @@
+package employeemanager;
+
+
+public class EmployeeManager {
+
+
+    public static void main(String[] args) {
+        
+        Login l1 = new Login();
+        
+        l1.setVisible(true);
+    }
+    
+}
---
+++ src/employeemanager/MyKeyListener.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package employeemanager;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author Ralty
+ */
+        class MyKeyListener implements KeyListener {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            // Perform your task when a key is pressed
+            System.out.println("www");
+  
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            // Not used in this example
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // Not used in this example
+        }
+    
+}
