135c85ada41ffa296022e5abe8ac1fa2524d1744
Raf <69535896+Rafee-M@users.noreply.github.com>
2024-06-09 14:00:08 +0000

    extends User.java

---
+++ src/employeemanager/Admin.java
@@ -0,0 +1,16 @@
+package employeemanager;
+
+/**
+ *
+ * @author Raafee-M
+ */
+
+public class Admin extends User{
+    
+    public Admin(String username, boolean isAdmin) {
+        super("admin", "admin432", true);
+    }
+    
+    
+    
+}
